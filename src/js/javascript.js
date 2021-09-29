c53746134434bf71d5e86feb36e125c061b5cd0b
Ignacio <jakrich@cifpfbmoll.eu>
2021-09-29 20:37:36 +0200

    create javascript.js

---
+++ src/js/javascript.js
@@ -0,0 +1,19 @@
+function changeBackgroundColor1() {
+    document.body.style.backgroundColor = "blue"; //cuando lo ejecuten cambia el color de fondo a azul
+}
+function changeBackgroundColor2() {
+    document.body.style.backgroundColor = "brown";//cuando lo ejecuten cambia el color de fondo a marron
+
+}function changeBackgroundColor3() {
+    document.body.style.backgroundColor = "orange";//cuando lo ejecuten cambia el color de fondo a naranja
+
+}function changeBackgroundColor4() {
+    document.body.style.backgroundColor = "green";//cuando lo ejecuten cambia el color de fondo a verde
+
+}function changeBackgroundColor5() {
+    document.body.style.backgroundColor = "red";//cuando lo ejecuten cambia el color de fondo a rojo
+}
+
+function normalBackgroundColor() {
+    document.body.style.backgroundColor = "white";//cuando lo ejecuten cambia el color de fondo a blanco
+}
